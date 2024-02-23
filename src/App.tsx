167a5d7dddaf7ea62a2b2274d180c87f9c01ae6e
Luankaio <luankaiosilveira45@hotmail.com>
2024-02-23 19:59:23 -0300

    falta_setas

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,27 @@ import {ReactComponent as Setad} from "./assets/arrow-right-wide-line.svg"
 import {ReactComponent as Setae} from "./assets/arrow-left-wide-line.svg"
 export const App = () => {
 
-  const [virado, valor] = useState(false);
+  const [virado, valor] = useState(true);
   
 
   return (
     <div className="Body">
       <div className='Cpai'>
-        <div className={virado ? 'Box-init' : 'Box-final'}>
-          <h1 className='Login'>Login</h1>
+        <div className={virado ? 'Box init' : 'Box final'}>
+          <h1 className='Login'>Criar conta</h1>
           <div className='Caixas'>
-            <input type="text" className='caixa senha' />
-            <input type="password" className='caixa email'/>
+            <input type="text" className='caixa email'/>
+            <input type="password" className='caixa senha' />
           </div>
         </div>
-        <div className={virado ? 'Box2-init' : 'Box2-final'}>
-          
+        <div className={virado ? 'Box init2' : 'Box final2'}>
+          <h1>oidsadsa</h1>
         </div >  
+        <div className='Ai'><div className='Botao b1' onClick={() => valor(true)}><Setae/></div></div>
+        <div className='Ai'><div className='Botao b2' onClick={() => valor(false)}><Setad/></div></div>
       </div>
       
-      <div className='Botao b1' onClick={() => valor(true)}><Setae/></div>
-      <div className='Botao b2' onClick={() => valor(false)}><Setad/></div>
+     
     </div>
   );
 }
