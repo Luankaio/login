82f17d02b26736b8be988ce802938beebcfa0d90
Luankaio <luankaiosilveira45@hotmail.com>
2024-02-24 03:59:42 -0300

    correcao_inicio_animacao__corrigir_botao_esquerdo_inicial

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,42 @@ import { useState } from 'react';
 import './App.css';
 import {ReactComponent as Setad} from "./assets/arrow-right-wide-line.svg"
 import {ReactComponent as Setae} from "./assets/arrow-left-wide-line.svg"
+import classNames from 'classnames';
+
 export const App = () => {
 
-  const [virado, valor] = useState(true);
+  const [virado, setVirado] = useState(true);
 
+  const [execInit, setExecInit] = useState(false)
+  const [execFinal, setExecFinal] = useState(false)
+
+  function handleExecInit() {
+    setExecInit(true)
+  setExecFinal(false)
+  }
+
+  function handleExecFinal() {
+    setExecInit(false)
+  setExecFinal(true)
+  }
 
   return (
     <div className="Body">
       <div className='Cpai'>
-        <div className={virado ? 'Box init' : 'Box final'}>
-          <h1 className='Login'>Criar conta</h1>
+        <div className={classNames('Box',execInit && 'init', execFinal && 'final')}>
+          <h1 className='Login'>Login</h1>
           <div className='Caixas'>
             <h1 className='femail'>Email</h1>
             <input type="email" className='caixa email'/>
             <input type="password" className='caixa senha' />
           </div>
         </div>
-        <div className={virado ? 'Box init2' : 'Box final2'}>
-          <h1>usuario</h1>
+        <div className={classNames('Box2',execInit &&  'init2', execFinal && 'final2')}>
+          {(execFinal||execInit) && <h1>usuario</h1>}
         </div >  
         
-        <div className='Ai'><div className='Botao b1' onClick={() => valor(true)}><Setae/></div></div>
-        <div className='Ai'><div className='Botao b2' onClick={() => valor(false)}><Setad/></div></div>
+        <div className='Ai'><div className='Botao b1' onClick={handleExecInit}><Setae/></div></div>
+        <div className='Ai'><div className='Botao b2' onClick={handleExecFinal}><Setad/></div></div>
       </div>
       
      
