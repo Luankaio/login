5f334781d3576c597ec47b59b5a7f1bc727ee0cc
Luankaio <luankaiosilveira45@hotmail.com>
2024-02-27 18:27:54 -0300

    formulações

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,33 @@ import { Giros } from './components/Giros'
 export const App = () => {
   const giro = Giros();
   const botao = () => {
-    return <div>          <button className='Botao b1' onClick={!giro.execInit&&!giro.execFinal?giro.vazio:giro.handleExecInit}><Setae/></button>
+    return <div> 
+    <button className='Botao b1' onClick={!giro.execInit&&!giro.execFinal?giro.vazio:giro.handleExecInit}><Setae/></button>
     <button className='Botao b2' onClick={giro.handleExecFinal}><Setad/></button> </div>
   }
   
   return (
     <div className="Body">
-      <div className='Cpai'>
-        <div className={classNames('Box',giro.execInit && 'init', giro.execFinal && 'final')}>
-          <h1 className='Login'>Login</h1>
-          <div className='Caixas'>
-            <h1 className='femail'>Email</h1>
-            <input type="email" className='caixa email'/>
-            <input type="password" className='caixa senha' />
+      <div className='CPai' >
+        <div className='Cfilho'> 
+          <div className={classNames('Box',giro.execInit && 'init', giro.execFinal && 'final')}>
+            <h1 className='Login'>Login</h1>
+            <div className='Caixas'>
+              
+              <input placeholder=' ' type="email" className='caixa email'/>
+              <h1 className='femail'>Email</h1>
+              <input type="password" placeholder= "oioi" className='caixa senha' />
+              <h1 className='fsenha'>Senha</h1>
+            </div>
+            
           </div>
+          {(giro.execFinal||giro.execInit) &&< div className={classNames('Box2',giro.execInit &&  'init2', giro.execFinal && 'final2')}>
+            <h1>usuario</h1>
+            
+          </div > }
           {botao ()}
         </div>
-        {(giro.execFinal||giro.execInit) &&< div className={classNames('Box2',giro.execInit &&  'init2', giro.execFinal && 'final2')}>
-           <h1>usuario</h1>
-          {botao ()}
-        </div > }
-        
       </div>
-      
      
     </div>
   );
